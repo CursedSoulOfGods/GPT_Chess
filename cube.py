95b3ad81ae2f88f821a717baece3d09970ca6b9b
Sumangalam Avtar Sharma <sumangalam_avtar@yahoo.com>
2023-04-16 00:52:00 +0530

    Fixed draw_cube function
    Signed-off-by: Sumangalam Avtar Sharma <sumangalam_avtar@yahoo.com>

--- cube.py
+++ cube.py
@@ -79,3 +79,40 @@ def rotate_cube(theta, axis):
 
 # Function to draw the cube
 def draw_cube(position):
+    x, y = position
+
+    # Draw the front face of the cube
+    pygame.draw.rect(screen, RED, (x-cube_size/2, y-cube_size/2, cube_size, cube_size))
+
+    # Draw the back face of the cube
+    pygame.draw.polygon(screen, GREEN, [(x-cube_size/2, y-cube_size/2), (x+cube_size/2, y-cube_size/2), (x+cube_size/2, y+cube_size/2), (x-cube_size/2, y+cube_size/2)])
+
+    # Draw the top face of the cube
+    pygame.draw.polygon(screen, BLUE, [(x-cube_size/2, y-cube_size/2), (x-cube_size/2+10, y-cube_size/2-10), (x+cube_size/2+10, y-cube_size/2-10), (x+cube_size/2, y-cube_size/2)])
+
+    # Draw the bottom face of the cube
+    pygame.draw.polygon(screen, YELLOW, [(x-cube_size/2, y+cube_size/2), (x-cube_size/2+10, y+cube_size/2-10), (x+cube_size/2+10, y+cube_size/2-10), (x+cube_size/2, y+cube_size/2)])
+
+    # Draw the left face of the cube
+    pygame.draw.polygon(screen, ORANGE, [(x-cube_size/2, y-cube_size/2), (x-cube_size/2+10, y-cube_size/2-10), (x-cube_size/2+10, y+cube_size/2-10), (x-cube_size/2, y+cube_size/2)])
+
+    # Draw the right face of the cube
+    pygame.draw.polygon(screen, PURPLE, [(x+cube_size/2, y-cube_size/2), (x+cube_size/2+10, y-cube_size/2-10), (x+cube_size/2+10, y+cube_size/2-10), (x+cube_size/2, y+cube_size/2)])
+
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    # Clear the screen
+    screen.fill((255, 255, 255))
+
+    # Draw the cube
+    draw_cube(cube_position)
+
+    # Update the screen
+    pygame.display.update()
+
+pygame.quit()
