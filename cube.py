3aeff45e69273a60ef830dba7c9baeb0241b2935
Sumangalam Avtar Sharma <sumangalam_avtar@yahoo.com>
2023-04-16 01:44:35 +0530

    Fixed Spacing and Colors
    Signed-off-by: Sumangalam Avtar Sharma <sumangalam_avtar@yahoo.com>

--- cube.py
+++ cube.py
@@ -11,21 +11,21 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("8x8 Cartesian Plane")
 
 # Define the size of each point and the spacing between them
-point_size = 10
-point_spacing = point_size * 2
+point_size = 25
+point_spacing = point_size * 1
 
 # Define the position of the plane
 plane_position = [size[0]/2, size[1]/2]
 
 # Define the colors of the points
-BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
 # Draw the points on the screen
 for x in range(8):
     for y in range(8):
         if (x + y) % 2 == 0:
-            rect_color = BLACK
+            rect_color = RED
         else:
             rect_color = WHITE
         rect = pygame.Rect(int(plane_position[0] + (x - 3.5) * point_spacing), int(plane_position[1] + (y - 3.5) * point_spacing), point_size, point_size)
